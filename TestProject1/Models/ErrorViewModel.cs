94edba325ddaacbde8091e06e9c242453fa14fa5
Muqum_dv24 <Muqum_dv24@STHQ012E-04>
2024-12-20 12:47:33 +0400

    Add project files.

---
+++ TestProject1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestProject1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
